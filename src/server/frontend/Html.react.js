2b32997cef428066598d90280a582589fadf8943
Delwiv <dev.louis.cathala@openmailbox.org>
2016-03-06 14:06:10 +0100

    First : WIP rebranding

--- src/server/frontend/Html.react.js
+++ src/server/frontend/Html.react.js
@@ -1,53 +1,48 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 
 export default class Html extends Component {
 
-  static propTypes = {
-    appCssFilename: PropTypes.string,
-    bodyHtml: PropTypes.string.isRequired,
-    googleAnalyticsId: PropTypes.string.isRequired,
-    helmet: PropTypes.object.isRequired,
-    isProduction: PropTypes.bool.isRequired,
-  };
+    static propTypes = {
+        appCssFilename: PropTypes.string,
+        bodyHtml: PropTypes.string.isRequired,
+        googleAnalyticsId: PropTypes.string.isRequired,
+        helmet: PropTypes.object.isRequired,
+        isProduction: PropTypes.bool.isRequired
+    };
 
-  render() {
-    const {
-      appCssFilename, bodyHtml, googleAnalyticsId, isProduction, helmet
-    } = this.props;
+    render() {
+        const {appCssFilename, bodyHtml, helmet} = this.props;
 
-    const linkStyles = appCssFilename &&
-      <link
-        href={appCssFilename}
-        rel="stylesheet"
-      />;
+        const linkStyles = appCssFilename && <link href={appCssFilename} rel="stylesheet"/>;
 
-    const analytics = isProduction && googleAnalyticsId !== 'UA-XXXXXXX-X' &&
-      <script
-        dangerouslySetInnerHTML={{ __html: `
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', '${googleAnalyticsId}', 'auto'); ga('send', 'pageview');` }}
-      />;
+        //     const analytics = isProduction && googleAnalyticsId !== 'UA-XXXXXXX-X' &&
+        //       <script
+        //         dangerouslySetInnerHTML={{ __html: `
+        // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        // ga('create', '${googleAnalyticsId}', 'auto'); ga('send', 'pageview');` }}
+        //       />;
 
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport" />
-          <meta content="ie=edge" httpEquiv="x-ua-compatible" />
-          {helmet.title.toComponent()}
-          {helmet.base.toComponent()}
-          {helmet.meta.toComponent()}
-          {helmet.link.toComponent()}
-          {helmet.script.toComponent()}
-          {linkStyles}
-          {analytics}
-        </head>
-        <body dangerouslySetInnerHTML={{ __html: bodyHtml }} />
-      </html>
-    );
-  }
+        return (
+            <html lang="en">
+                <head>
+                    <meta charSet="utf-8"/>
+                    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport"/>
+                    <meta content="ie=edge" httpEquiv="x-ua-compatible"/>
+                    {helmet.title.toComponent()}
+                    {helmet.base.toComponent()}
+                    {helmet.meta.toComponent()}
+                    {helmet.link.toComponent()}
+                    {helmet.script.toComponent()}
+                    {linkStyles}
+                </head>
+                <body dangerouslySetInnerHTML={{
+                    __html: bodyHtml
+                }}/>
+            </html>
+        );
+    }
 
 }
