2e327a9e0b1a0fa6c09e48343ffcd1818b001ac2
Delwiv <dev.louis.cathala@openmailbox.org>
2016-03-15 12:54:35 +0100

    fix sync between origin and upstream

---
+++ src/native/config.js
@@ -0,0 +1,9 @@
+/* eslint-disable eol-last, quotes, quote-props */
+export default {
+  "defaultLocale": "en",
+  "firebaseUrl": "https://este.firebaseio.com",
+  "locales": [
+    "cs",
+    "en"
+  ]
+};
